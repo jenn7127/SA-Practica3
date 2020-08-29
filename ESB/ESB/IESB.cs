d9e484ecc086c1be5d407611d0790636c33f7e8c
Jennifer <jenny7127@gmail.com>
2020-08-29 10:27:20 -0600

    add ESB2

--- ESB/ESB/IESB.cs
+++ ESB/ESB/IESB.cs
@@ -10,7 +10,7 @@ namespace ESB
 {
     // NOTA: puede usar el comando "Rename" del menú "Refactorizar" para cambiar el nombre de interfaz "IService1" en el código y en el archivo de configuración a la vez.
     [ServiceContract]
-    public interface IService1
+    public interface IESB
     {
 
         [OperationContract]
